c6b723de6721055661522a56dedeeb8451e3ef76
Rock <faiz2rock@gmail.com>
2018-01-26 22:02:33 +0530

    Fixes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //Load HTTP module
-require('newrelic');
+// require('newrelic');
 var express = require('express');
 const app = express();
 var http = require("http");
 const path = require('path');
-const routes = require('express').Router();
+// const routes = require('express').Router();
 var port = process.env.PORT|| 8000 ;
 var serveStatic = require('serve-static');
 
@@ -15,13 +15,13 @@ console.log("\n\n Dist path is "+DIST_DIR);*/
 /*console.log("\n\nMain path is "+appDir);
 app.use(express.static(__dirname, '/assets'));*/
 
-app.use('/css', express.static(__dirname + '/assets/homepage/'));
+// app.use('/css', express.static(__dirname + '/assets/homepage/'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/src/index.html'));
+// });
 console.log("\n\npath is "+__dirname);
-
+app.use(express.static(__dirname + '/dist'));
 // Print URL for accessing server
 app.listen(port);
 var date =new Date();
